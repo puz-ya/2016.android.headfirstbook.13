2ee4bff2500b9d748a33570a2500064e993ae4cb
puz_ya <puzino.y.a@gmail.com>
2017-01-13 13:28:28 +0300

    Add: notification through Handler->Toast.

--- app/src/main/java/com/yd/joke/DelayedMessageService.java
+++ app/src/main/java/com/yd/joke/DelayedMessageService.java
@@ -2,7 +2,9 @@ package com.yd.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
 
 /**
@@ -13,16 +15,23 @@ import android.util.Log;
  */
 public class DelayedMessageService extends IntentService {
      public static final String EXTRA_MESSAGE = "intentService";
+    private Handler mHandler;
 
     public DelayedMessageService(){
         super("DelayedMessageService");
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startID){
+        mHandler = new Handler();
+        return super.onStartCommand(intent, flags, startID);
+    }
+
+    @Override
     protected void onHandleIntent(Intent intent){
         synchronized (this){
             try{
-                wait(10000);
+                wait(5000);
             }catch (InterruptedException ex){
                 ex.printStackTrace();
             }
@@ -34,5 +43,11 @@ public class DelayedMessageService extends IntentService {
 
     private void showText(final String str){
         Log.v("DelayedService", str);
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Service is running", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
