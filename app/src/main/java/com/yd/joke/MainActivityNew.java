5adec62b2287313e3bddf15e05f7b18830106956
puz_ya <puzino.y.a@gmail.com>
2017-01-13 15:43:56 +0300

    Added: Location, ServiceConnection, checkMiles(), uses-permission.

--- app/src/main/java/com/yd/joke/MainActivityNew.java
+++ app/src/main/java/com/yd/joke/MainActivityNew.java
@@ -1,13 +1,79 @@
 package com.yd.joke;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
 
 public class MainActivityNew extends Activity {
+
+    private OdometrService mOdometrService;
+    private boolean mIsBind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_new);
+
+        checkMiles();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Intent intent = new Intent(this, OdometrService.class);
+        //create if not exist and BIND
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            OdometrService.OdometrBinder odometrBinder = (OdometrService.OdometrBinder) iBinder;
+            mOdometrService = odometrBinder.getOdometr();
+            mIsBind = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mIsBind = false;
+        }
+    };
+
+    private void checkMiles(){
+        final TextView textView = (TextView) findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if(mOdometrService != null){
+                    distance = mOdometrService.getMiles();
+                }
+
+                String string = String.format(Locale.getDefault(), "%1$,.2f miles", distance);
+                textView.setText(string);
+                handler.postDelayed(this, 1000); //every second check distance
+            }
+        });
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+
+        if(mIsBind){
+            unbindService(mServiceConnection);
+            mIsBind = false;
+        }
     }
 }
--- app/src/main/java/com/yd/joke/OdometrService.java
+++ app/src/main/java/com/yd/joke/OdometrService.java
@@ -2,13 +2,21 @@ package com.yd.joke;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 
 public class OdometrService extends Service {
 
     //interface Binder
-    private final IBinder mIBinder = new OdometrBinder();
+    public final IBinder mIBinder = new OdometrBinder();
+
+    private static double mDistance;
+    private static Location mLastLocation = null;
 
     public class OdometrBinder extends Binder {
         OdometrService getOdometr(){
@@ -20,7 +28,48 @@ public class OdometrService extends Service {
     }
 
     @Override
+    public void onCreate(){
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if(mLastLocation == null){
+                    mLastLocation = location;
+                }
+                mDistance += location.distanceTo(mLastLocation);
+                mLastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+                //don't use
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+                //don't use
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                //don't use
+            }
+        };
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //here we NEED TO CHECK PERMISSIONS API 21+
+        try{
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, listener);
+        }catch (SecurityException ex){
+            Log.v("OdometrService", "Error: " + ex.getMessage());
+        }
+    }
+
+    @Override
     public IBinder onBind(Intent intent) {
         return mIBinder;
+    }
+
+    public double getMiles() {
+        return mDistance / 1609.344;    //retard units (miles)
     }
 }
